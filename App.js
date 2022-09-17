696842f3a407a2b20af00bd705d038c46dc4dc88
ahmedmohamed520 <frontenddeveloper12020@gmail.com>
2022-09-17 13:49:44 +0200

    build app layout and header

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import Header from "./components/Header";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [todos, setTodos] = useState([
+        { text: "buy coffee", key: 1 },
+        { text: "create an app", key: 2 },
+        { text: "play on the switch", key: 3 },
+    ]);
+
+    return (
+        <View style={styles.container}>
+            <Header />
+            <View style={styles.content}>
+                {/* to do form */}
+                <View style={styles.list}>
+                    <FlatList
+                        data={todos}
+                        renderItem={({ item }) => {
+                            return <Text>{item.text}</Text>;
+                        }}
+                    />
+                </View>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+    content: {
+        padding: 40,
+    },
+    list: {
+        marginTop: 20,
+    },
 });
