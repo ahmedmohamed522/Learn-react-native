d5dbeb3962e6cb6626babe1aab264e11dd2efb3e
ahmedmohamed520 <frontenddeveloper12020@gmail.com>
2022-09-17 18:28:10 +0200

    flexbox lesson

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.boxOne}>One</Text>
+            <Text style={styles.boxTwo}>Two</Text>
+            <Text style={styles.boxThree}>Three</Text>
+            <Text style={styles.boxFour}>Four</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingTop: 40,
+        backgroundColor: "#777",
+    },
+    boxOne: { backgroundColor: "violet", padding: 10, flex: 1 },
+    boxTwo: { backgroundColor: "gold", padding: 20 },
+    boxThree: { backgroundColor: "coral", padding: 15 },
+    boxFour: { backgroundColor: "skyblue", padding: 25 },
 });
